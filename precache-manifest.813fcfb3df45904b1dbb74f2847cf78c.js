f16476f0e6a83eecaa8d624f36ab7cce7496af83
TipaMag <perynsky@gmail.com>
2020-04-21 16:12:17 +0300

    Updates

---
+++ precache-manifest.813fcfb3df45904b1dbb74f2847cf78c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020b63a017b5c070c8682ac07fbdb501",
+    "url": "/covid19-tracker-app/index.html"
+  },
+  {
+    "revision": "35139ce301c9038e29b0",
+    "url": "/covid19-tracker-app/static/css/main.ce5b566b.chunk.css"
+  },
+  {
+    "revision": "1c3d4bb22e0bad2837bc",
+    "url": "/covid19-tracker-app/static/js/2.f8c349e1.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid19-tracker-app/static/js/2.f8c349e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35139ce301c9038e29b0",
+    "url": "/covid19-tracker-app/static/js/main.3164308b.chunk.js"
+  },
+  {
+    "revision": "2e2d952393bbceff53e4",
+    "url": "/covid19-tracker-app/static/js/runtime-main.e17fe4eb.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker-app/static/media/logo.d7265326.png"
+  }
+]);
